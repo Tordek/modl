7b6a720a1042c5e67eca73c7ff200ff6a93331cd
Tordek <kedrot@gmail.com>
2019-02-13 12:32:58 -0300

    Make it able to run scripts.

--- modl.py
+++ modl.py
@@ -6,44 +6,46 @@ import modl_parser
 import modl_scanner
 import modl_interpreter
 
-# WHY GLOBALS?!
-hadError = False
 
 def main(args):
     if len(args) > 2:
         print("Usage: {} [script]".format(sys.args[0]), file=sys.stderr)
         exit(-1)
     elif len(args) == 2:
-        run_file(args[1])
-        if hadError:
-            exit(65)
+        exit(run_file(args[1]))
     else:
         run_prompt()
         
 
 def run_file(path):
     with codecs.open(path, encoding="utf8") as script:
-        run(script.read())
-        hadError = False
-        
+        env = modl_interpreter.Environment()
+        env = env.set("!", "!")
+        interpreter = modl_interpreter.Interpreter()
 
+        scanner = modl_scanner.Scanner(command)
+        parser = modl_parser.Parser(scanner.scan_tokens())
+
+        for statement in parser.program():
+            (result, env) = interpreter.interpret(statement, env)
+        
+        return result
+        
+        
 def run_prompt():
     env = modl_interpreter.Environment()
     env = env.set("!", "!")
     while True:
         try:
-            env = run(input("> "), env)
+            command = input("> ")
+            scanner = modl_scanner.Scanner(command)
+            parser = modl_parser.Parser(scanner.scan_tokens())
+            interpreter = modl_interpreter.Interpreter()
+            (result, env) = interpreter.interpret(parser.statement(), env)
+            print(result)
+            hadError = False
         except Exception as e:
             print(traceback.format_exc())
-            
-
-def run(command, env):
-    scanner = modl_scanner.Scanner(command)
-    parser = modl_parser.Parser(scanner.scan_tokens())
-    interpreter = modl_interpreter.Interpreter()
-    (result, env) = interpreter.interpret(parser.statement(), env)
-    print(result)
-    return env
     
 
 def error(line, message):
