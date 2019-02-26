281d598d35d0d5c265d8940c0060d29e2f05f402
Tordek <kedrot@gmail.com>
2019-02-25 22:30:34 -0300

    Inflate coverage% by moving this away

--- bin/modl_cli.py
+++ bin/modl_cli.py
@@ -1,8 +1,60 @@
-#!/bin/env python
-
 import sys
 import os
 sys.path.insert(0, os.path.abspath("."))
-from modl import cli
 
-cli.main(sys.argv)
+import codecs
+import traceback
+
+from modl.parser import Parser
+from modl.scanner import Scanner
+from modl import interpreter
+
+
+def main(args):
+    if len(args) > 2:
+        print("Usage: {} [script]".format(sys.args[0]), file=sys.stderr)
+        exit(-1)
+    elif len(args) == 2:
+        exit(run_file(args[1]))
+    else:
+        run_prompt()
+
+
+def run_file(path):
+    with codecs.open(path, encoding="utf8") as script:
+        env = interpreter.get_default_env()
+        scanner = Scanner(command)
+        parser = Parser(scanner.scan_tokens())
+
+        for statement in parser.program():
+            (result, env) = interpreter.interpret(statement, env)
+
+        return result
+
+
+def run_prompt():
+    env = interpreter.get_default_env()
+    while True:
+        try:
+            command = input("> ")
+            scanner = Scanner(command)
+            parser = Parser(scanner.scan_tokens())
+            (result, env) = interpreter.interpret(parser.statement(), env)
+            print(result)
+            hadError = False
+        except Exception as e:
+            print(traceback.format_exc())
+
+
+def error(line, message):
+    report(line, "", message)
+
+
+def report(line, where, message):
+    global hadError
+    print("[line {}] Error{}: {}".format(line, where, message), file=sys.stderr)
+    hadError = True
+
+
+if __name__ == "__main__":
+    main(sys.argv)
